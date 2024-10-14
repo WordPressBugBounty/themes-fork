c1c06303e0111329b90997c24bde6296e4b8d36f
Sean Murphy <sean@iamseanmurphy.com>
2024-10-14 01:05:22 +0000

    1.0.8

---
+++ fork/assets/js/build/design-pack-notice.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'dea34a3be620274acf5d');
---
+++ fork/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/fork',
+        'pretty_version' => 'v1.0.8',
+        'version' => '1.0.8.0',
+        'reference' => 'be0deaf0b3e4b4fa5db0f7b1a6b0dbdec0655970',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/fork' => array(
+            'pretty_version' => 'v1.0.8',
+            'version' => '1.0.8.0',
+            'reference' => 'be0deaf0b3e4b4fa5db0f7b1a6b0dbdec0655970',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.29',
+            'version' => '3.3.29.0',
+            'reference' => '4d63bc81ec0357c8675dac1e35511fd81104605d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
