9f75c643f7c97814be38a2613e462f5cb822d88b
Sean Murphy <sean@iamseanmurphy.com>
2024-11-08 10:30:32 +0000

    1.0.9

--- fork/CHANGELOG.md
+++ fork/CHANGELOG.md
@@ -1,3 +1,7 @@
+##### [Version 1.0.9](https://github.com/Codeinwp/fork/compare/v1.0.8...v1.0.9) (2024-11-08)
+
+- Updated dependencies
+
 ##### [Version 1.0.8](https://github.com/Codeinwp/fork/compare/v1.0.7...v1.0.8) (2024-08-28)
 
 ### Bug Fixes
--- fork/vendor/composer/installed.php
+++ fork/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/fork',
-        'pretty_version' => 'v1.0.8',
-        'version' => '1.0.8.0',
-        'reference' => 'be0deaf0b3e4b4fa5db0f7b1a6b0dbdec0655970',
+        'pretty_version' => 'v1.0.9',
+        'version' => '1.0.9.0',
+        'reference' => 'e373ef7eba061259a7bed5d5dcef8b6ca86de43f',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'codeinwp/fork' => array(
-            'pretty_version' => 'v1.0.8',
-            'version' => '1.0.8.0',
-            'reference' => 'be0deaf0b3e4b4fa5db0f7b1a6b0dbdec0655970',
+            'pretty_version' => 'v1.0.9',
+            'version' => '1.0.9.0',
+            'reference' => 'e373ef7eba061259a7bed5d5dcef8b6ca86de43f',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.29',
-            'version' => '3.3.29.0',
-            'reference' => '4d63bc81ec0357c8675dac1e35511fd81104605d',
+            'pretty_version' => '3.3.37',
+            'version' => '3.3.37.0',
+            'reference' => '73e9010fb4e30ad8835f00a0df802733f98c4094',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
